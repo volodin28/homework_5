5148d84982fb4cb45a595d70aa13655efa7f683c
Viacheslav Volodin <volodin28@gmail.com>
2023-04-27 21:44:01 +0300

    homework done

---
+++ mysite/teachers/management/commands/generate_teachers.py
@@ -0,0 +1,16 @@
+from django.core.management.base import BaseCommand
+from faker import Faker
+from teachers.models import Teacher
+
+faker = Faker()
+
+
+class Command(BaseCommand):
+
+    def add_arguments(self, parser):
+        parser.add_argument("quantity", type=int, nargs='?', default=100)
+
+    def handle(self, quantity, **options):
+        for _ in range(quantity):
+            t = Teacher.objects.create(first_name=faker.first_name(), last_name=faker.last_name())
+            self.stdout.write(self.style.SUCCESS('Successfully created teacher with ID "%s"' % t.id))
